aee73b7fc167a1ca3de6e795094547f5151a1fb5
tbhatnagar18 <34261284+tbhatnagar18@users.noreply.github.com>
2020-06-18 17:57:22 +0530

    Updating groovy script for TestOne Project

--- job-dsl/testGroovy.groovy
+++ job-dsl/testGroovy.groovy
@@ -1,18 +1,5 @@
-job('NodeJS example') {
-    scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-        }
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
-    wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-    }
+job('TestOne Project') {
     steps {
-        shell("npm install")
+        shell("echo... This is a test...")
     }
 }
