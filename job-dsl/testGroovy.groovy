9b377b09540b81a11d0bc2be59d1a6f93fbd85e8
Tushar Bhatnagar <tbhatnagar@bravurasolutions.com>
2020-06-22 12:35:54 +0530

    Changes for node JS project - 6

--- job-dsl/testGroovy.groovy
+++ job-dsl/testGroovy.groovy
@@ -1,4 +1,4 @@
-job('Node JS Example'){
+job('Tushar/Node JS Example'){
 	scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
